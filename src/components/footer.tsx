ee2123fd780da7cb6ee376e257a39180668612eb
Faiz Shaikh <faizshaikh72740@gmail.com>
2024-10-20 23:32:37 +0530

    added correct copyright year in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            © 2023 Faiz Shaikh. All rights reserved.
+            © {new Date().getFullYear()} Faiz Shaikh. All rights reserved.
           </p>
         </div>
         <div className="flex items-center space-x-4">
